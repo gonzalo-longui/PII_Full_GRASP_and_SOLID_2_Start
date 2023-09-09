1e923301bb8e34ae473060254648f1ea72697f87
longuigon <longuigon+git@gmail.com>
2023-09-09 18:34:45 -0300

    hecho

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ConsolePrinter
+    {
+        public static void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+            foreach (Step step in recipe.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+        //Principio SRP: Esta clase tiene la única responsabilidad de imprimir las recetas
+        //Princpio Expert: Esta clase tiene toda la información que necesita para imprimir las recetas
+    }
+}
